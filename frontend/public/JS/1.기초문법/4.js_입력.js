17805232ea7307d8a7f201644ba3324f48ffcd82
504 <504@DESKTOP-J6BU8A7>
2023-01-09 15:29:23 +0900

    day07 문제2~6

--- frontend/public/JS/1.기초문법/4.js_입력.js
+++ frontend/public/JS/1.기초문법/4.js_입력.js
@@ -13,9 +13,12 @@
 	문자형 입력 받을때
 		prompt('메시지')	
  */
+/*
 //1. 입력메시지에 대한 확인/취소 버튼 결과를 변수에 저장
 let result1 = confirm('실행하시겠습니까?')
 console.log( '결과1 : ' + result1 )
 //2. 입력메시지에 입력된 데이터를 변수에 저장
-let result2 = prompt('데이터 입력')
-console.log( '결과2 : ' + result2 )
+let result3 = prompt('데이터 입력')
+console.log( '결과2 : ' + result3 )
+*/
+
