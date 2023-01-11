0ca940b9ac6345e60e78f85f26b0e7f564c0b80e
504 <504@DESKTOP-J6BU8A7>
2023-01-11 17:58:35 +0900

    day 09 과제3 하는중

---
+++ frontend/public/JS/0.과제/과제3.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+console.log('JS')	//확인용
+
+let 방명록 = '<tr> <th>번호</th> <th>방문록</th></tr>'; // 결과 누적
+
+function gstBook(){
+	
+	//let 방명록 = '<tr> <th>번호</th> <th>방문록</th></tr>';
+	let contentArray = []
+	let text1Value = document.querySelector ('.text1').value
+	
+
+		
+	
+	
+	
+	//누적된 변수 table에 넣어주기 	
+	document.querySelector('.con').innerHTML = 방명록
+	
+} 
