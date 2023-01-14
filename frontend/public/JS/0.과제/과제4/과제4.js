af366e18484d71c19830089a4beffc74fb631131
dbwl9 <dbwl9@DESKTOP-STKA34T>
2023-01-14 16:02:28 +0900

    JS 과제 4

--- frontend/public/JS/0.과제/과제4/과제4.js
+++ frontend/public/JS/0.과제/과제4/과제4.js
@@ -17,38 +17,20 @@ console.log('js') //확인용
 		1.없는 도서명 불가능
 		2.대여중인 도서가 아니면 불가능
 */
-let 도서목록 = ['혼자공부하는자바','이것이자바다','열혈 C언어']
+let 도서목록 = ['혼자공부하는자바','이것이자바다','열혈C언어']
 	
 let 대여목록 = ['혼자공부하는자바']
-
+//console.log('2.'+도서목록) 확인용
 //도서등록
-
-function btn(ex){ // f s
+function btn(){ // f s
 
 	let content = document.querySelector('.content').value
 	let bookName = document.querySelector('.bookName').value
 	 // <input> 값 변수에 저장
-	도서목록.push(content)
-	console.log('2.:'+도서목록)
+	도서목록.push(bookName)
+  console.log('3:'+ 도서목록)
 	
-	let bno =`<tr>
-				<td>번호</td>
-				<td>도서목록</td>
-				<td>대여여부</td>
-				<td>비고</td>
-			</tr>`
-			
-	for(let i = 0; i <도서목록.length;i++){//테이블 목록 추가 나중에 추가
-		bno +=`<tr>
-				<td>${i}</td>
-				<td>${도서목록[i]}</td>
-				<td>대여</td>
-				<td><button>삭제</button></td>
-			</tr>`
-		
-	}	document.querySelector ('.table').innerHTML = bno
-	
-	if (도서목록.indexOf(ex) <= 0){// 만약 도서목록 중복이면 알림창뜨기
+	if (도서목록.indexOf() >= 0){// 만약 도서목록 중복이면 알림창뜨기
 		alert('이미 등록된 도서입니다.')
 		return;
 	}//if 1 e
@@ -58,10 +40,31 @@ function btn(ex){ // f s
 		alert('11자 미만으로 입력해주세요.')
 		return;	
 	}//else if e
+	printContent()
+	도서목록.push() //위 조건 달성시 저장)
+}//f e 
+//삭제버튼 함수
+function del(){
+	도서목록.splice(도서목록.indexOf(),1)
+	printContent()
+}
 
-	도서목록.push(ex) //위 조건 달성시 저장
-	
+//출력 함수
+function printContent(){// 등록, 삭제 함수 사용	
+	let bno =`<tr>
+				<td>번호</td>
+				<td>도서목록</td>
+				<td>대여여부</td>
+				<td>비고</td>
+			</tr>`
+			
+		for(let i = 0; i <도서목록.length;i++){ //테이블 도서목록 뒤 추가
+		bno +=`<tr>
+				<td>${i+1}</td>
+				<td>${도서목록[i]}</td>
+				<td>대여</td>
+				<td><button onclick="del(${i})">삭제</button></td>
+			</tr>`
+	}	
 	document.querySelector ('.table').innerHTML = bno
 }	//f e
-
-//f e
