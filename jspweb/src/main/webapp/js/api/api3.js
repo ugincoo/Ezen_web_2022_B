e1d85f57851a0d0ecbb9d82fe4deb0bca30fb476
ujin <dbwl95@naver.com>
2023-03-21 23:02:02 +0900

    API 과제 - 마커 클럭스터러 수정 완료

--- jspweb/src/main/webapp/js/api/api3.js
+++ jspweb/src/main/webapp/js/api/api3.js
@@ -34,53 +34,53 @@ var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 //------------------------ 주소-좌표 변환  ------------------------
+// 마커 클러스터러를 생성합니다 
+var clusterer = new kakao.maps.MarkerClusterer({
+    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+    minLevel: 6 // 클러스터 할 최소 지도 레벨 
+});
 
-	// 주소-좌표 변환 객체를 생성합니다
 
+getgeocoder(); 
 function getgeocoder(){
 	$.ajax({
-		url : "https://api.odcloud.kr/api/3035882/v1/uddi:5fae3cf5-bc15-4eba-87d8-8289b74e659b_201912202015?page=1&perPage=150&serviceKey=d6G0imt5Vz21ZWQfaccWnoPsQ1pfEOSbMols03Bwa%2FgS88FT9gmyDyGD5hm5%2Fm2BFG%2FH%2BCDLkFzTLP3ZE1VTrg%3D%3D",	
-		async : 'false',
-		method : "get",
-		success : (re)=>{
-			console.log(re);
-			re.data.forEach( (o)=>{
-							// 주소로 좌표를 검색합니다			
-					
-			geocoder.addressSearch(o.주소, function(result, status) {
-			
-			    // 정상적으로 검색이 완료됐으면 
-			     if (status === kakao.maps.services.Status.OK) {
-			
-			        let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-					let info = {title : o.약국명 ,latlng: new kakao.maps.LatLng(result[0].y, result[0].x) }
-					console.log(info);
-				
+	url : "https://api.odcloud.kr/api/3035882/v1/uddi:5fae3cf5-bc15-4eba-87d8-8289b74e659b_201912202015?page=1&perPage=150&serviceKey=d6G0imt5Vz21ZWQfaccWnoPsQ1pfEOSbMols03Bwa%2FgS88FT9gmyDyGD5hm5%2Fm2BFG%2FH%2BCDLkFzTLP3ZE1VTrg%3D%3D" ,
+	method : "get" ,
+	async : 'false' ,
+	success : (re)=>{
+		console.log(re);
+		// 데이터에서 좌표 값을 가지고 마커를 표시합니다
+        // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
+        
+		// 주소-좌표 변환 객체를 생성합니다
+		let geocoder = new kakao.maps.services.Geocoder();
 
-			        // 결과값으로 받은 위치를 마커로 표시합니다
-			        let marker = new kakao.maps.Marker({
-			            map: map,
-			            position: coords,
-			            image: markerImage
-			        });
-			       
-			        //클릭 이벤트
-					kakao.maps.event.addListener(marker, 'click', function() {
-      					// 마커 위에 인포윈도우를 표시합니다
-      					alert(o.약국명+"입니다.");
+		
+		for(let i = 0 ; i < re.data.length ; i++ ){
+			// 주소로 좌표를 검색합니다
+			geocoder.addressSearch(re.data[i].주소, function(result, status) {
+
+					    // 정상적으로 검색이 완료됐으면 
+					     if (status === kakao.maps.services.Status.OK) {
+							let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+				 	        let marker = new kakao.maps.Marker({
+					            position :coords ,
+					            image : markerImage // 마커 이미지 
+					        });
+
+					 	clusterer.addMarker(marker);
+
+						 // 클릭이벤트
+						kakao.maps.event.addListener(marker, 'click', function() {
+      					alert(re.data[i].약국명+"입니다.\n" + "주소 : " + re.data[i].주소 +"\n전화번호 : " + re.data[i].대표전화 );
 					});
-			        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-			        marker.setMap(map);
-		   			 } 
-
-				});   
-
-
-			})
-			
+			    	} 
+				});
+			}
 		}
 	})
-	
 }
 
 //------------------------마커 이미지 [O]  ------------------------
@@ -92,63 +92,7 @@ var imageSrc = '/jspweb/img/약국.png', // 마커이미지의 주소입니다
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
     markerPosition = new kakao.maps.LatLng(37.3218778, 126.8308848); // 마커가 표시될 위치입니다
 
-//------------------------마커 여러개 [O]   ------------------------   
-	let geocoder = new kakao.maps.services.Geocoder();
-    // 마커 클러스터러를 생성합니다 
-    var clusterer = new kakao.maps.MarkerClusterer({
-        map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
-        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
-        minLevel: 10 // 클러스터 할 최소 지도 레벨 
-    });
 
- 	
-    // 데이터를 가져오기 위해 jQuery를 사용합니다
-    // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
-    $.get(
-		"https://api.odcloud.kr/api/3035882/v1/uddi:5fae3cf5-bc15-4eba-87d8-8289b74e659b_201912202015?page=1&perPage=150&serviceKey=d6G0imt5Vz21ZWQfaccWnoPsQ1pfEOSbMols03Bwa%2FgS88FT9gmyDyGD5hm5%2Fm2BFG%2FH%2BCDLkFzTLP3ZE1VTrg%3D%3D",
-		 (re)=> {
-        // 데이터에서 좌표 값을 가지고 마커를 표시합니다
-        // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
-        var markers = $(re.data).map(function(i,o) {
-			
-			geocoder.addressSearch(o.주소, function(result, status) {
-			
-			    // 정상적으로 검색이 완료됐으면 
-			     if (status === kakao.maps.services.Status.OK) {
-			
-			        let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-					let info = {title : o.약국명 ,latlng: new kakao.maps.LatLng(result[0].y, result[0].x) }
-					console.log(info);
-				
-
-			        // 결과값으로 받은 위치를 마커로 표시합니다
-			        var marker = new kakao.maps.Marker({
-			            position: coords,
-			            image: markerImage
-			        });
-			       
-			        //클릭 이벤트
-					kakao.maps.event.addListener(marker, 'click', function() {
-      					// 마커 위에 인포윈도우를 표시합니다
-      					alert(o.약국명+"입니다.");
-					});
-			        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-			       marker.setMap(map);
-			       
-		   			 } 
-					 
-					return marker;  
-				});   
-				
-            
-        });
-		
-        // 클러스터러에 마커들을 추가합니다
-        clusterer.addMarkers(markers);
-       console.log(markers);
-    });
-
-    
 //------------------------마커 생성 ------------------------
 var markerPosition  = new kakao.maps.LatLng(37.3218778, 126.8308848); 
 // 마커를 생성합니다
